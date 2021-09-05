3949a25fa366d0820e77b1c6fdf2859533c3da07
wc <2019012617@bistu.edu.cn>
2021-09-05 12:20:52 +0800

    微信服务器与开发者服务武器通信的接口

--- service/src/main/java/cn/edu/bistu/wx/service/WxMiniApi.java
+++ service/src/main/java/cn/edu/bistu/wx/service/WxMiniApi.java
@@ -1,4 +1,4 @@
-package cn.edu.bistu.weixin;
+package cn.edu.bistu.wx.service;
 
 
 import com.alibaba.fastjson.JSONObject;
@@ -37,6 +37,29 @@ public interface WxMiniApi {
      */
     JSONObject authCode2Session(String appId, String secret, String jsCode);
 
+    /**
+     * 获取小程序的accessToken接口，如果本地缓存了access-token且没有过期，直接从本地获取，否则访问微信接口。
+     * 之所以要缓存到本地，是因为每天请求微信接口的次数是有限制的。
+     *
+     * @param appId  小程序appId
+     * @param secret 小程序secret
+     * @return 返回访问微信服务器的accessToken
+     */
     String getAccessToken(String appId, String secret);
+
+    /**
+     *
+     * @param sig 签名
+     * @param times 时间戳
+     * @param nonce 随机数
+     * @return 验证通过返回true，否则false
+     */
+    boolean check(String sig, String times, String nonce);
+
+    /**
+     * 小程序发送模板消息给指定用户
+     * @param openId 目标用户的openId
+     */
+    void sendSubscribeMsg(String openId);
 
 }
