6344d9230508b09897193d0ea00bc26e58c62278
wc <2019012617@bistu.edu.cn>
2021-09-05 12:16:28 +0800

    访问日志新增http请求体的MIME类型

--- service/src/main/java/cn/edu/bistu/common/interceptor/LogInterceptor.java
+++ service/src/main/java/cn/edu/bistu/common/interceptor/LogInterceptor.java
@@ -32,6 +32,7 @@ public class LogInterceptor implements HandlerInterceptor {
         log.info("访问时间：" + now);
         log.info("访问接口：" + request.getRequestURI());
         log.info("访问方式：" + request.getMethod());
+        log.info("MIME类型：" + request.getContentType());
         return true;
     }
 
