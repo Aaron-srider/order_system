713c993254f6445301fefa6945c79748d2141bcb
wc <2019012617@bistu.edu.cn>
2021-09-06 22:09:45 +0800

    修改userInfoCompletion接口：整理代码，稍微改变接口逻辑。

--- service/src/main/java/cn/edu/bistu/auth/service/UserServiceImpl.java
+++ service/src/main/java/cn/edu/bistu/auth/service/UserServiceImpl.java
@@ -1,12 +1,13 @@
 package cn.edu.bistu.auth.service;
 
 import cn.edu.bistu.auth.mapper.UserMapper;
-import cn.edu.bistu.auth.service.UserService;
+import cn.edu.bistu.auth.mapper.UserRoleMapper;
+import cn.edu.bistu.common.exception.UserNotRegisteredException;
 import cn.edu.bistu.constants.ResultCodeEnum;
-import cn.edu.bistu.model.common.Result;
 import cn.edu.bistu.model.entity.auth.User;
-import cn.edu.bistu.model.UserInfoChecker;
+import cn.edu.bistu.model.entity.auth.UserRole;
 import cn.edu.bistu.model.vo.UserVo;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,56 +21,33 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements Us
     UserMapper userMapper;
 
     @Autowired
-    UserInfoChecker userInfoChecker;
+    UserRoleMapper userRoleMapper;
 
     @Override
-    public UserVo findByOpenId(String openId) {
-        UserVo userVo = userMapper.selectByOpenId(openId);
-        return userVo;
-    }
+    public void userInfoCompletion(UserVo userVo, Long roleId) {
 
-    @Override
-    public Result userInfoCompletion(UserVo userVo) {
+        QueryWrapper<User> wrapper = new QueryWrapper<>();
+        wrapper.eq("id", userVo.getId());
+        User user = userMapper.selectOne(wrapper);
 
-        //检查前端信息是否完整
-        boolean frontInfoComplete = false;
-        userInfoChecker.setUser(userVo);
-        try {
-            frontInfoComplete = userInfoChecker.checkFrontUserInfo();
-        } catch (NoSuchFieldException e) {
-            log.error("requiredUserInfo.properties配置文件属性配置错误");
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
+        //用户没注册
+        if (user == null) {
+            throw new UserNotRegisteredException("user id: " + userVo.getId(), ResultCodeEnum.USER_NOT_REGISTERED);
         }
 
-        if(frontInfoComplete) {
-            //检查用户是否注册
-            User user1 = userMapper.selectById(userVo.getId());
-            if(user1 == null) {
-                return new Result().codeEnum(ResultCodeEnum.USER_UNREGISTERED);
-            }
-
-            //检查用户信息是否完善，已经完善就跳过
-            Integer isComplete = userMapper.selectById(userVo.getId()).getInfoComplete();
-
-            //如果未完善，完善信息
-            if (isComplete.equals(0)) {
-
-                userVo.setInfoComplete(1);
-                userMapper.updateUserById(userVo);
-                return Result.ok();
-
-            } else {
-                return new Result().codeEnum(ResultCodeEnum.USER_INFO_COMPLETED);
-            }
-
-        } else {
-            return new Result().codeEnum(ResultCodeEnum.FRONT_DATA_MISSING);
+        //用户已经完善过信息了
+        if (user.isUserInfoCompleted()) {
+            throw new UserNotRegisteredException("user id: " + userVo.getId(), ResultCodeEnum.USER_INFO_COMPLETED);
         }
 
+        //更新用户表
+        userMapper.userInfoComplete(userVo);
+
+        //向UserRole表中插入数据
+        UserRole userRole = new UserRole();
+        userRole.setRoleId(roleId);
+        userRole.setUserId(userVo.getId());
+        userRoleMapper.insert(userRole);
     }
-
-
-
 
 }
