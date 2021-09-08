7f2d430785ab2ba9a458eb165e0983d858bcc0bd
wc <2019012617@bistu.edu.cn>
2021-09-08 09:16:10 +0800

    新增mapper

---
+++ service/src/main/java/cn/edu/bistu/auth/mapper/RoleMapper.java
@@ -0,0 +1,9 @@
+package cn.edu.bistu.auth.mapper;
+
+import cn.edu.bistu.model.entity.auth.Role;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleMapper extends BaseMapper<Role> {
+}
