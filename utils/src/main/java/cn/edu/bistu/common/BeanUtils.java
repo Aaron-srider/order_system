218b5918290b2a6c3275ba8b8091f11eb7b4466f
wc <2019012617@bistu.edu.cn>
2021-09-08 09:19:29 +0800

    新增getDeclaredField方法，替代Class.getDeclaredField方法

--- utils/src/main/java/cn/edu/bistu/common/BeanUtils.java
+++ utils/src/main/java/cn/edu/bistu/common/BeanUtils.java
@@ -1,11 +1,42 @@
 package cn.edu.bistu.common;
 
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.lang.reflect.Field;
 import java.text.SimpleDateFormat;
 import java.util.*;
 
+@Slf4j
 public class BeanUtils {
+
+    /**
+     * 该静态方法用于代替Class类的getDeclaredField方法，在目标类及其所有父类中查找一个属性，
+     * 方法中委托目标类及其父类的getDeclaredField方法，在各层级查找指定属性，使用while循环
+     * 和接口Class.getSuperclass()沿着继承树依次向上遍历所有父类。
+     * @param clazz 目标类
+     * @param name 属性名字符串
+     * @return 返回找到的属性Field对象
+     * @throws NoSuchFieldException 若目标类中及其所有父类都找不到该属性，抛出该异常
+     */
+    public static Field getDeclaredField(Class clazz, String name) throws NoSuchFieldException {
+        Field declaredField = null;
+        while (clazz != null &&
+                !clazz.getName().toLowerCase().equals("java.lang.object")) {//当父类为null的时候说明到达了最上层的父类(Object类).
+
+            try {
+                declaredField  = clazz.getDeclaredField(name);
+            } catch (NoSuchFieldException e) {
+                log.debug(clazz.getTypeName() + " has no such field called " + name);
+                //得到父类,然后赋给自己
+                clazz = clazz.getSuperclass();
+                continue;
+            }
+            log.debug("field " + name + " was found in class " + clazz.getTypeName());
+            return declaredField;
+        }
+        throw new NoSuchFieldException("field " + name + " not found");
+    }
 
     public static boolean isFieldNull(Object obj, Field field) throws IllegalAccessException {
         field.setAccessible(true);
