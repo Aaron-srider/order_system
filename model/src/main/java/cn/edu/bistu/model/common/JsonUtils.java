af3db4ecd1b7726866d080ffa127579e197f31bd
wc <2019012617@bistu.edu.cn>
2021-09-10 20:29:08 +0800

    为创建符合标准的单个Dao JSONObject提供便利

---
+++ model/src/main/java/cn/edu/bistu/model/common/JsonUtils.java
@@ -0,0 +1,23 @@
+package cn.edu.bistu.model.common;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+
+/**
+ * 为JSONObject的构造提供一定遍历，方便符合DaoResult的规范
+ */
+public class JsonUtils {
+
+    public static <T> void setResult(JSONObject jsonObject, T result) {
+        DaoResult<T> daoResult = new DaoResultImpl<>();
+        daoResult.setValue(jsonObject);
+        daoResult.setResult(result);
+    }
+    public static void setDetailInfo(JSONObject jsonObject, JSONObject detailInfo) {
+        DaoResult daoResult = new DaoResultImpl<>();
+        daoResult.setValue(jsonObject);
+        daoResult.setDetailInfo(detailInfo);
+    }
+
+
+}
