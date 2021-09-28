ef09951b495978e32b4f0383126e6531725e990b
wc <2019012617@bistu.edu.cn>
2021-09-28 22:18:41 +0800

    更新getCommonInfo，新增获取WorkOrderStatus常用数据字典，供前端映射使用

--- service/src/main/java/cn/edu/bistu/dept/service/DeptServiceImpl.java
+++ service/src/main/java/cn/edu/bistu/dept/service/DeptServiceImpl.java
@@ -6,6 +6,8 @@ import cn.edu.bistu.model.common.result.ServiceResultImpl;
 import cn.edu.bistu.model.entity.College;
 import cn.edu.bistu.model.entity.Major;
 import cn.edu.bistu.model.entity.SecondaryDept;
+import cn.edu.bistu.model.entity.WorkOrderStatus;
+import cn.edu.bistu.workOrder.mapper.WorkOrderDao;
 import com.alibaba.fastjson.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -18,16 +20,21 @@ public class DeptServiceImpl implements DeptService {
     @Autowired
     DeptDao deptDao;
 
+    @Autowired
+    WorkOrderDao workOrderDao;
+
     @Override
     public ServiceResult<JSONObject> getAllDeptCollegeMajor() {
         List<Major> majorList = deptDao.getMajorMapper().selectList(null);
         List<SecondaryDept> secondaryDeptList = deptDao.getSecondaryDeptMapper().selectList(null);
         List<College> collegeList = deptDao.getCollegeMapper().selectList(null);
+        List<WorkOrderStatus> workOrderStatusList = workOrderDao.getWorkOrderStatusMapper().selectList(null);
 
         JSONObject jsonObject = new JSONObject();
         jsonObject.put("majorList", majorList);
         jsonObject.put("secondaryDeptList", secondaryDeptList);
         jsonObject.put("collegeList", collegeList);
+        jsonObject.put("workOrderStatusList", collegeList);
 
         return new ServiceResultImpl<JSONObject>(jsonObject);
     }
